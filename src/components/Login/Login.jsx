cb1b15bb45db98175176018066f61034d894e662
sfk-aayan <190041116faiyaz@gmail.com>
2022-10-30 22:02:16 +0600

    Added fancy stuff

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,9 +4,97 @@ import Helmet from 'react-helmet';
 import cryptocurrency from '../../images/cryptocurrency.png';
 import Tilt from 'react-parallax-tilt';
 import 'tachyons';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { useCallback } from "react";
 
+const particleOptions = {
+    background: {
+        color: {
+            value: "",
+        },
+    },
+    fpsLimit: 120,
+    interactivity: {
+        events: {
+            onClick: {
+                enable: true,
+                mode: "push",
+            },
+            onHover: {
+                enable: true,
+                mode: "repulse",
+            },
+            resize: true,
+        },
+        modes: {
+            push: {
+                quantity: 4,
+            },
+            repulse: {
+                distance: 200,
+                duration: 0.4,
+            },
+        },
+    },
+    particles: {
+        color: {
+            value: "#ffffff",
+        },
+        links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+        },
+        collisions: {
+            enable: true,
+        },
+        move: {
+            directions: "none",
+            enable: true,
+            outModes: {
+                default: "bounce",
+            },
+            random: false,
+            speed: 6,
+            straight: false,
+        },
+        number: {
+            density: {
+                enable: true,
+                area: 800,
+            },
+            value: 80,
+        },
+        opacity: {
+            value: 0.5,
+        },
+        shape: {
+            type: "square",
+        },
+        size: {
+            value: { min: 5, max: 10 },
+        },
+    },
+    detectRetina: true,
+}
 
 export const Login = (props) => {
+
+    const particlesInit = useCallback(async (engine) => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(engine);
+    }, []);
+    
+    const particlesLoaded = useCallback(async (container) => {
+        await console.log(container);
+    }, []);
+
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
 
@@ -18,6 +106,12 @@ export const Login = (props) => {
 
     return (
         <div >
+        <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={particleOptions}
+      />
         <Helmet bodyAttributes={{style: 'background-color : #001529'}} />
         <Tilt tiltMaxAngleY={2} tiltMaxAngleX={5}>
                 <div className='Tilt left br2 shadow-2 pa3 ma4 mt3' style={{ height: '8em', width: '8em'}}>
